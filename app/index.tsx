0068bfee480bb57e450348d6f8e792c1ffb02b9b
Sunil <sunilkumawat7717@gmail.com>
2024-08-31 00:38:16 +0530

    chore: Update dependencies and add nativewind plugin

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,36 @@
+import beachImage from '@/assets/meditation-images/beach.webp'
+import AppGradient from '@/components/AppGradient'
+import CustomButton from '@/components/CustomButton'
+import { useRouter } from 'expo-router'
+import { StatusBar } from "expo-status-bar"
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const index = () => {
+const App = () => {
+  const rounter = useRouter()
   return (
-    <View>
-      <Text>index</Text>
+    <View className='flex-1'>
+      <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+        <SafeAreaView className='flex-1 px-1  justify-between'>
+          <View>
+            <Text className="text-center text-white font-bold text-4xl">
+              Simple Meditation
+            </Text>
+            <Text className="text-center text-white font-regular text-2xl mt-3">
+              Simplifying Meditation for Everyone
+            </Text>
+          </View>
+          <View>
+             <CustomButton onPress={()=> rounter.push("/nature-meditate")} title='Get Started' />
+          </View>
+          <StatusBar style='light' />
+        </SafeAreaView>
+        </AppGradient>
+      </ImageBackground>   
     </View>
   )
 }
 
-export default index
+export default App
